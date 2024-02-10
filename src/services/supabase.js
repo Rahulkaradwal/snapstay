bc826793589b7b76295c7ff3e3e3c888ae79e51f
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-02-10 02:35:07 -0500

    feat: add backened

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://mhuakitottmdslslzrjc.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1odWFraXRvdHRtZHNsc2x6cmpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc1NDg2NTIsImV4cCI6MjAyMzEyNDY1Mn0.e9FJQts3JUke3nCyFAIbvtRapZ17MSB3txYU4TJLxb0';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
